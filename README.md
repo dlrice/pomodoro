ee468874b01c8f90916f0c1b7b20f2515aaad5f6
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-09-21 16:27:30 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+Pomodoro
+--------
+
+
+solemn.py from [Notification Sounds](https://notificationsounds.com) and is provided under a [Creative Commons Attribution license](https://creativecommons.org/licenses/by/4.0/legalcode).
---
+++ pomodoro.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import os
+import subprocess
+from tqdm import tqdm
+from time import sleep
+
+AUDIO_FILE = 'solemn.mp3'
+DURATION = 25
+
+def notify():
+    os.system("osascript -e 'display notification \"Pomodoro session complete!\" with title \"Done!\"'")
+    subprocess.call(['afplay', AUDIO_FILE])
+
+def main():
+    for i in tqdm(range(DURATION), leave=True, bar_format='{bar} {total} minutes'):
+        sleep(0.060)
+    notify()
+
+if __name__ == '__main__':
+    main()
