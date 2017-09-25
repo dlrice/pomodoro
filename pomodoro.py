9c1d290f8782fcd76be3750dc6d7564610c24ff3
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-09-25 12:55:26 +0100

    Adding command line arg for duration of pomodor.

--- pomodoro.py
+++ pomodoro.py
@@ -3,18 +3,31 @@ import os
 import subprocess
 from tqdm import tqdm
 from time import sleep
+import sys
 
 AUDIO_FILE = 'solemn.mp3'
-DURATION = 25
 
 def notify():
     os.system("osascript -e 'display notification \"Pomodoro session complete!\" with title \"Done!\"'")
     subprocess.call(['afplay', AUDIO_FILE])
 
-def main():
-    for i in tqdm(range(DURATION), leave=True, bar_format='{bar} {n} | {total} minutes', ncols=DURATION*2):
-        sleep(60)
+def pomodoro(duration):
+    s = '{} min '.format(duration)
+    args = {
+        'leave': True,
+        'bar_format': '|{bar}| ' + s,
+        'ncols': max(duration*2, 50)
+    }
+    niterations = 25
+    sleep_duration = 60 * duration / niterations
+    for i in tqdm(range(niterations), **args):
+        sleep(sleep_duration)
     notify()
+
+def main():
+    args = sys.argv
+    duration = int(args[1]) if len(args) > 1 else 25
+    pomodoro(duration)
 
 if __name__ == '__main__':
     main()
