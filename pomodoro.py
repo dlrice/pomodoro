e5d72606bddaaf2aa5e1f3f8efab348d9385a5d7
Daniel Rice <Daniel.L.Rice@gmail.com>
2017-11-26 22:02:37 +0000

    Handle term signal and print progress so far.

--- pomodoro.py
+++ pomodoro.py
@@ -4,30 +4,49 @@ import subprocess
 from tqdm import tqdm
 from time import sleep
 import sys
+import signal
 
 AUDIO_FILE = 'solemn.mp3'
 
-def notify():
-    os.system("osascript -e 'display notification \"Pomodoro session complete!\" with title \"Done!\"'")
-    subprocess.call(['afplay', AUDIO_FILE])
 
-def pomodoro(duration):
-    s = '{} min '.format(duration)
-    args = {
-        'leave': True,
-        'bar_format': '|{bar}| ' + s,
-        'ncols': max(duration*2, 50)
-    }
-    niterations = 25
-    sleep_duration = 60 * duration / niterations
-    for i in tqdm(range(niterations), **args):
-        sleep(sleep_duration)
-    notify()
+class Pomodoro(object):
+
+    def __init__(self, duration):
+        self._duration = duration
+        signal.signal(signal.SIGINT, self.signal_handler)
+
+    def signal_handler(self, signal, frame):
+        print('\nCancelled after {} minutes'.format(self._progress/60))
+        sys.exit(0)
+
+
+    def notify(self):
+        os.system("osascript -e 'display notification \"Pomodoro session complete!\" with title \"Done!\"'")
+        subprocess.call(['afplay', AUDIO_FILE])
+
+    def start(self):
+        duration = self._duration
+
+        s = '{} min '.format(duration)
+        args = {
+            'leave': True,
+            'bar_format': '|{bar}| ' + s,
+            'ncols': max(duration*2, 50)
+        }
+        niterations = 25
+        sleep_duration = 60 * duration / niterations
+        for i in tqdm(range(niterations), **args):
+            sleep(sleep_duration)
+            self._progress = (i + 1) * sleep_duration
+        self.notify()
+
 
 def main():
     args = sys.argv
     duration = int(args[1]) if len(args) > 1 else 25
-    pomodoro(duration)
+    p = Pomodoro(duration)
+    p.start()
+
 
 if __name__ == '__main__':
     main()
